83ff07e20a0cc691c9266ed12fbf36cec0cf041f
kccraig <40079447+kccraig@users.noreply.github.com>
2020-09-18 18:28:47 -0400

    json

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 const requestURL = 'js/notes.json';
-
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
         console.table(jsonObject);
-    const notes = jsonObject['mynotes'];
-    console.log(notes);
-    })
-    for (let i = 0; i < 6; i++) {
-        for (let i = 0; i < notes.length; i++) {
-const dw = notes
-           console.log(notes[0]);
-        }}
+        const notes = jsonObject['mynotes'];
+        console.log(notes);
 
-        /*
-     var name = document.createTextNode(rentals[i].name);
-     */
+        for (let i = 0; i < notes.length; i++) {
+            let list = document.createElement('ol');
+            let item = document.createElement('li');
+            item.textContent = notes[i].dswwt;
+            list.appendChild(item);
+
+
+
+
+        }
+    })
