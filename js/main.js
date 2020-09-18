51f58ff4f148625f859271a05c3a288c0555f892
kccraig <40079447+kccraig@users.noreply.github.com>
2020-09-18 11:40:43 -0400

    add json

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-alert("hi");
+const requestURL = 'js/notes.json';
 
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+    const notes = jsonObject['mynotes'];
+    console.log(notes);
+    })
+    
