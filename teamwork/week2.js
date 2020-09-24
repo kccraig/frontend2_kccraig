b4850b724a9dd987b585c9469bb6affaf38d7382
kccraig <40079447+kccraig@users.noreply.github.com>
2020-09-24 12:15:01 -0400

    adding

--- teamwork/week2.js
+++ teamwork/week2.js
@@ -5,5 +5,15 @@ document.getElementById('empty').innerHTML =  "\"" +  text + "\"";
 }
 
 function numsum() {
+    const end = document.getElementById("numbox1").value;
+    for (let i = 0; i <= end; i++) {
     
+
+    
+    document.getElementById('sumbox').innerHTML = i;
+
+        
+    }
+    
+ 
 }
