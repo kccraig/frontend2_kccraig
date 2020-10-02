fe5db6e4008c4c23c248656a8bdff9e310693d85
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-02 10:57:51 -0400

    more notes

--- mynotes/mynotes.js
+++ mynotes/mynotes.js
@@ -1,2 +1,5 @@
 const date = new Date();
-document.getElementById('timeplace').innerHTML = date;
+const month = date.getMonth();
+const day = date.getDate();
+const year = date.getFullYear();
+document.getElementById('timeplace').innerHTML = month + "/" + day + "/" + year;
