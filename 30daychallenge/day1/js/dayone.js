bf2f8e85ab0eddfdc05ee7bfe6cc73d72c7629e4
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-02 07:56:24 -0400

    weeks

--- 30daychallenge/day1/js/dayone.js
+++ 30daychallenge/day1/js/dayone.js
@@ -1,14 +1,4 @@
 
 window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="$e.keyCode}"]`);
-    const key = document.querySelector(`.key[data=key="${e.keyCode}"]);
-    if (!audio)return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-    setTimeout(function() {
-        
-    })
-}
-
-
+    console.log(e.keycode);
+});
