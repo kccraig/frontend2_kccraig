2722f42e91d42e088a77ada0d9d0a27a319d5028
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-02 11:28:19 -0400

    object

--- mynotes/mynotes.js
+++ mynotes/mynotes.js
@@ -3,3 +3,15 @@ const month = date.getMonth();
 const day = date.getDate();
 const year = date.getFullYear();
 document.getElementById('timeplace').innerHTML = month + "/" + day + "/" + year;
+
+
+const car = {
+    make: 'ford',
+    model: 'falcon',
+    year: '1967',
+    similar: ['mustang', 'fairlane', 'focus'],
+    speedofcar() {
+        return 'bullit is the best';
+    }
+};
+console.table(car);
