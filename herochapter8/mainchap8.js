4c927c8abc81bce0baa2068838d36c2cfd78c771
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-07 17:13:49 -0400

    dd

--- herochapter8/mainchap8.js
+++ herochapter8/mainchap8.js
@@ -5,22 +5,23 @@ function makeHero(event) {
     event.preventDefault();
     const hero={};
     hero.name = form.heroName.value;
-    alert(JSON.stringify(hero));
-    return hero;
-    
-    hero.powers = [];
-    for (let i=0; i< form.powers.length; i++) {
-        if (form.powers[i].checked) {
-            hero.powers.push(form.powers[i].value);
-        }
-    }
-    hero.realName = form.realName.value;
-
     hero.category = form.category.value;
     hero.age = form.age.value;
     hero.city = form.city.value;
     hero.origin = form.origin.value;
+    hero.powers = [];
+    
+   
+    // for (let i=0; i< form.powers.length; i++) {
+    //     if (form.powers[i].checked) {
+    //         hero.powers.push(form.powers[i].value);
+    //     }
+    // }
+    hero.realName = form.realName.value;
 
+   
+    alert(JSON.stringify(hero));
+    return hero;
 }
 // document.forms.hero.powers[0].checked = true;
 
