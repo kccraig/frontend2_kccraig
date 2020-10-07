2218eda0bf357fef4f799b11ad7ea39884db5f54
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-07 13:36:13 -0400

    search go

--- quiz/search.js
+++ quiz/search.js
@@ -1,2 +1,27 @@
-const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'), false);
+const input = box.elements.searchInput;
+
+input.addEventListener('focus', () => console.log('focused'), false);
+input.addEventListener('blur', () => console.log('blurred'), false);
+input.addEventListener('change', () => console.log('changed'), false);
+
+const entered = document.forms['box'];
+box.addEventListener ('submit', search, false);
+function search() {
+    alert(' Form Submitted');
+}
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+input.value = 'Search Here';
+u=input.addEventListener('focus', function() {
+    if (input.value=== 'Search Here') {
+        input.value = ''
+    }
+}, false);
+input.addEventListener('blur', function() {
+    if(input.value === '') {
+        input.value = 'Search Here';
+    
+    }
+}, false);
