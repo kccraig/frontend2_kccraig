abc25459689d4dafcffa64c11e6652a220d735ff
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-07 12:50:21 -0400

    index

--- quiz/main.js
+++ quiz/main.js
@@ -17,5 +17,22 @@ document.getElementById('fruit').innerHTML = fruittalk;
 //      alert("cant find it");
 
 //  }
- const input = form.elements.searchInput;
- input.addEventListener('focus', () => alert('focused'), false);
+//  const input = form.elements.searchInput;
+//  input.addEventListener('focus', () => alert('focused'), false);
+
+ const list = new Set();
+ list.add(3);
+ list.add(2) .add(4) .add(7);
+ const letters = new Set('apples');
+ list.delete(4);
+ const musicSet = new Set().add('jorge').add('michael').add('paul');
+console.log(musicSet);
+const musicArray = Array.from(musicSet);
+console.log(musicArray);
+console.log(musicArray.length);
+ console.log(letters);
+ console.log(list);
+
+ sayHi = () => {
+     alert("HI!");
+ }
