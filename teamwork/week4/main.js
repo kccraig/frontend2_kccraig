1a6864d1db9034bd25d9152cd7cc994401a9647e
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-08 18:34:57 -0400

    add

---
+++ teamwork/week4/main.js
@@ -0,0 +1,12 @@
+const clickParagraph = document.getElementById('click');
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
+
+addEventListener('click', (event) => {
+    if (event.shiftKey) {
+        console.log('A Shifty Click!');
+    }
+});
+
+addEventListener('touchend', () => console.log('Touch stopped'));
