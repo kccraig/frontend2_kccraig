039071b870ce6f230bcb0e37fdae53099e0c7dee
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-08 22:38:01 -0400

    tic tac

--- teamwork/week4/main.js
+++ teamwork/week4/main.js
@@ -1,14 +1,28 @@
-const clickParagraph = document.getElementById('click');
-clickParagraph.addEventListener('click',() => console.log('click') );
-clickParagraph.addEventListener('mousedown',() => console.log('down') );
-clickParagraph.addEventListener('mouseup',() => console.log('up') );
+const gameBoardDiv = document.getElementById('gameBoard');
+const divs= document.querySelectorAll('#gameBoard>div')''
+const player1 - 'x';
+const player2 = 'o';
+let currentPlayer = player1;
+divs.forEach()element)=> {
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 addEventListener('click', (event) => {
-    if (event.shiftKey) {
-        console.log('A Shifty Click!');
+    if (event.click) {
+        console.log('keyclicked');
     }
 });
 
-addEventListener('touchend', () => console.log('Touch stopped'));
 
-document.nodeName;
+
