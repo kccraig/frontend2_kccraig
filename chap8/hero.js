65e0e3bfd9be34440e97e729293457c384d218bf
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-12 09:55:00 -0400

    heros

---
+++ chap8/hero.js
@@ -0,0 +1,12 @@
+// “const form = document.forms['hero'];
+// form.addEventListener('submit', makeHero, false);”
+
+const form = document.forms['hero'];
+form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); 
+    const hero = {};
+    hero.name = form.heroName.value;
+    alert(JSON.stringify(hero));
+}
