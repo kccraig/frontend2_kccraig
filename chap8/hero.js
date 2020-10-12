db2552703e51db113d8d976c380c962312f1e365
kccraig <40079447+kccraig@users.noreply.github.com>
2020-10-12 12:43:39 -0400

    form

--- chap8/hero.js
+++ chap8/hero.js
@@ -9,10 +9,22 @@ function makeHero(event) {
     const hero = {};
     hero.name = form.heroName.value;
     alert(JSON.stringify(hero));
+    hero.realName = form.realName.value;
+    console.log(form.powers);
+    hero.powers = [];
+    for (let i=0; i < form.powers.length; i++) {
+        if (form.powers[i].checked) {
+            hero.powers.push(form.powers[i].value);
+        }
+    }
+    console.log(hero.powers);
+    hero.powers = [...form.powers].filter(box=> box.checked).map(box => box.value);
+    console.log(hero.powers);
 }
-function showPage() {
-    const word= document.getElementById('wordPlace').innerHTML;
-    const password = "go";
-    if (word = passWord) {
-        window.open("https://www.w3schools.com/jsref/met_win_open.asp");
-}
+
+// function showPage() {
+//     const word= document.getElementById('wordPlace').innerHTML;
+//     const password = "go";
+//     if (word = passWord) {
+//         window.open("https://www.w3schools.com/jsref/met_win_open.asp");
+// }
